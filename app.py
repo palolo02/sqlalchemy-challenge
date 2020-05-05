3eed9b66d5e387e079c8d65fe3b3ef7775edde8a
Paolo Vega <pao02esc@gmail.com>
2020-05-04 22:00:20 -0500

    Update in dates notebook and app

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
+# Paolo Vega
+# SQLAlchemy Challenge
+# Bootcamp
+# Versión 1.0.0 May-03-2020
+# Versión 1.0.1 May-03-2020
+# Versión 1.0.2 May-03-2020 (updates and comments)
+# Versión 1.0.3 May-04-2020
+
 #################################################
 # Import Modules
 #################################################
 
 from flask import Flask, jsonify
-import os
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -21,10 +28,6 @@ from sqlalchemy import create_engine, func, inspect
 #################################################
 # DB Connection
 #################################################
-print("#################################################")
-print(os.getcwd())
-
-
 
 app = Flask(__name__)
 
@@ -65,11 +68,18 @@ def precipitation():
         Measurement = Base.classes.measurement
         # Create our session (link) from Python to the DB
         session = Session(engine)
-        last_date = session.query(func.max(Measurement.date).label('last_date')).subquery('t')
-        # Select fields for precipitaton per day
-        # Need to update dynamic filter for 1 year *************************
+        # get the last date in the DB
+        max_date = session.query(func.max(Measurement.date).label('last_date')).all()[0][0]
+        # Convert to date
+        max_date = dt.datetime.strptime(max_date,"%Y-%m-%d")
+        # Calculate  the date - one year ago
+        one_year_ago = max_date - dt.timedelta(days=365)
         sel = [Measurement.date,func.sum(Measurement.prcp)]
-        precipitation = session.query(*sel).filter(Measurement.date >= '2016-08-23').filter(Measurement.date <= last_date.c.last_date).group_by(Measurement.date).order_by(Measurement.date).all()
+        precipitation = session.query(*sel).\
+            filter(Measurement.date >= func.strftime("%Y-%m-%d",one_year_ago)).\
+            filter(Measurement.date <= func.strftime("%Y-%m-%d",max_date)).\
+            group_by(Measurement.date).\
+            order_by(Measurement.date).all()
         # Transform into dataframe
         precipitation = pd.DataFrame(precipitation, columns=["day","prcp"])
         # update datatype for day
@@ -118,7 +128,12 @@ def temperature():
         session = Session(engine)
         last_date = session.query(func.max(Measurement.date).label('last_date')).subquery('t')
         sel_waihee_data = [Measurement.date, Measurement.tobs]
-        waihee_data = session.query(*sel_waihee_data).filter(Measurement.station == 'USC00519281').filter(Measurement.date >= '2016-08-23').filter(Measurement.date <= last_date.c.last_date).group_by(Measurement.date).order_by(Measurement.date).all()
+        waihee_data = session.query(*sel_waihee_data).\
+            filter(Measurement.station == 'USC00519281').\
+            filter(Measurement.date >= func.strftime("%Y-%m-%d",one_year_ago)).\
+            filter(Measurement.date <= func.strftime("%Y-%m-%d",max_date)).\
+            group_by(Measurement.date).\
+            order_by(Measurement.date).all()
         # Transform into dataframe
         waihee_df = pd.DataFrame(waihee_data, columns=["day","temp"])
         # update datatype for day
@@ -170,7 +185,11 @@ def range_temperature(start,end):
     # Create our session (link) from Python to the DB
     session = Session(engine)
     sel_list_temp = [Measurement.date, func.min(Measurement.tobs).label('min_temp'), func.avg(Measurement.tobs).label('avg_temp'), func.max(Measurement.tobs).label('max_temp') ]
-    temp = session.query(*sel_list_temp).filter(Measurement.date >= start).filter(Measurement.date <= end).group_by(Measurement.date).order_by(Measurement.date).all()
+    temp = session.query(*sel_list_temp).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).\
+        group_by(Measurement.date).\
+        order_by(Measurement.date).all()
     # Transform into dataframe
     temp_df = pd.DataFrame(temp, columns=["day","Min Temp","Avg Temp","Max Temp"])
     # update datatype for day
