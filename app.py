f2d9314d8ceac0d68866412d5c07962090aece28
Paolo Vega <pao02esc@gmail.com>
2020-05-07 23:23:34 -0500

    Last comments and adjustments

--- app.py
+++ app.py
@@ -3,8 +3,11 @@
 # Bootcamp
 # Versión 1.0.0 May-03-2020
 # Versión 1.0.1 May-03-2020
-# Versión 1.0.2 May-03-2020 (updates and comments)
+# Versión 1.0.2 May-03-2020
 # Versión 1.0.3 May-04-2020
+# Versión 1.0.4 May-05-2020
+# Versión 1.0.5 May-06-2020
+# Versión 1.0.6 May-07-2020
 
 #################################################
 # Import Modules
@@ -47,10 +50,11 @@ def welcome():
                 <h1>Welcome to the Weather Vacation Analysis!</h1>
                 <h2>Available Routes:</h2>
                 <hr />
-                <h3>/api/v1.0/precipitation</h3>
-                <h3>/api/v1.0/stations</h3>
-                <h3>/api/v1.0/tobs</h3>
-                <h3>/api/v1.0/50/80</h3>
+                <h3><a href="./api/v1.0/precipitation">/api/v1.0/precipitation</a></h3>
+                <h3><a href="./api/v1.0/stations">/api/v1.0/stations</a></h3>
+                <h3><a href="./api/v1.0/tobs">/api/v1.0/tobs</a></h3>
+                <h3><a href="./api/v1.0/2017-01-01">/api/v1.0/2017-01-01</a></h3>
+                <h3><a href="./api/v1.0/2017-01-01/2017-12-31">/api/v1.0/2017-01-01/2017-12-31</a></h3>
             </body>
         </html>
         '''
@@ -86,7 +90,7 @@ def precipitation():
         precipitation["day"] = pd.to_datetime(precipitation["day"])
         # update index to handle plot easily
         precipitation.set_index("day",inplace=True)
-        return jsonify(precipitation.to_json())
+        return jsonify({"content":precipitation.to_json()})
     except:
         return jsonify({"error":" There is an error with the query. Try agan later"}), 404
 
@@ -108,7 +112,7 @@ def stations():
         active_stations_df = pd.DataFrame(active_stations, columns=["station","rows"])
         active_stations_df.set_index("station",inplace=True)
         active_stations_df["rows"] = active_stations_df['rows'].map('{:,}'.format)
-        return jsonify(active_stations_df.to_json())
+        return jsonify({"content":active_stations_df.to_json()})
     except:
         return jsonify({"error":" There is an error with the query. Try agan later"}), 404
 
@@ -126,8 +130,14 @@ def temperature():
         Measurement = Base.classes.measurement
         # Create our session (link) from Python to the DB
         session = Session(engine)
-        last_date = session.query(func.max(Measurement.date).label('last_date')).subquery('t')
+        # get the last date in the DB
+        max_date = session.query(func.max(Measurement.date).label('last_date')).all()[0][0]
+        # Convert to date
+        max_date = dt.datetime.strptime(max_date,"%Y-%m-%d")
+        # Calculate  the date - one year ago
+        one_year_ago = max_date - dt.timedelta(days=365)
         sel_waihee_data = [Measurement.date, Measurement.tobs]
+
         waihee_data = session.query(*sel_waihee_data).\
             filter(Measurement.station == 'USC00519281').\
             filter(Measurement.date >= func.strftime("%Y-%m-%d",one_year_ago)).\
@@ -141,7 +151,7 @@ def temperature():
         # update index to handle plot easily
         waihee_df.set_index("day",inplace=True)
         # See sample data
-        return jsonify(waihee_df.to_json())
+        return jsonify({"content":waihee_df.to_json()})
     except:
         return jsonify({"error":" There is an error with the query. Try agan later"}), 404
 
@@ -149,55 +159,61 @@ def temperature():
 
 @app.route("/api/v1.0/<start>")
 def start_temperature(start):
-    print(f"parameter: {start} - {type(start)}")
-    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-    # reflect an existing database into a new model
-    Base = automap_base()
-    # reflect the tables
-    Base.prepare(engine, reflect=True)
-    # Save references to each table
-    Station = Base.classes.station
-    Measurement = Base.classes.measurement
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-    sel_list_temp = [Measurement.date, func.min(Measurement.tobs).label('min_temp'), func.avg(Measurement.tobs).label('avg_temp'), func.max(Measurement.tobs).label('max_temp') ]
-    temp = session.query(*sel_list_temp).filter(Measurement.date >= start).group_by(Measurement.date).order_by(Measurement.date).all()
-    # Transform into dataframe
-    temp_df = pd.DataFrame(temp, columns=["day","Min Temp","Avg Temp","Max Temp"])
-    # update datatype for day
-    temp_df["day"] = pd.to_datetime(temp_df["day"])
-    # update index to handle plot easily
-    temp_df.set_index("day",inplace=True)
-    # See sample data
-    return jsonify(temp_df.to_json())
+    try:
+        print(f"parameter: {start} - {type(start)}")
+        engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+        # reflect an existing database into a new model
+        Base = automap_base()
+        # reflect the tables
+        Base.prepare(engine, reflect=True)
+        # Save references to each table
+        Station = Base.classes.station
+        Measurement = Base.classes.measurement
+        # Create our session (link) from Python to the DB
+        session = Session(engine)
+        sel_list_temp = [Measurement.date, func.min(Measurement.tobs).label('min_temp'), func.avg(Measurement.tobs).label('avg_temp'), func.max(Measurement.tobs).label('max_temp') ]
+        temp = session.query(*sel_list_temp).filter(Measurement.date >= start).group_by(Measurement.date).order_by(Measurement.date).all()
+        # Transform into dataframe
+        temp_df = pd.DataFrame(temp, columns=["day","Min Temp","Avg Temp","Max Temp"])
+        # update datatype for day
+        temp_df["day"] = pd.to_datetime(temp_df["day"])
+        # update index to handle plot easily
+        temp_df.set_index("day",inplace=True)
+        # See sample data
+        return jsonify({"content":temp_df.to_json()})
+    except:
+        return jsonify({"error":" There is an error with the query. Try agan later"}), 404
 
 
 @app.route("/api/v1.0/<start>/<end>")
 def range_temperature(start,end):
-    engine = create_engine("sqlite:///Resources/hawaii.sqlite")
-    # reflect an existing database into a new model
-    Base = automap_base()
-    # reflect the tables
-    Base.prepare(engine, reflect=True)
-    # Save references to each table
-    Station = Base.classes.station
-    Measurement = Base.classes.measurement
-    # Create our session (link) from Python to the DB
-    session = Session(engine)
-    sel_list_temp = [Measurement.date, func.min(Measurement.tobs).label('min_temp'), func.avg(Measurement.tobs).label('avg_temp'), func.max(Measurement.tobs).label('max_temp') ]
-    temp = session.query(*sel_list_temp).\
-        filter(Measurement.date >= start).\
-        filter(Measurement.date <= end).\
-        group_by(Measurement.date).\
-        order_by(Measurement.date).all()
-    # Transform into dataframe
-    temp_df = pd.DataFrame(temp, columns=["day","Min Temp","Avg Temp","Max Temp"])
-    # update datatype for day
-    temp_df["day"] = pd.to_datetime(temp_df["day"])
-    # update index to handle plot easily
-    temp_df.set_index("day",inplace=True)
-    # See sample data
-    return jsonify(temp_df.to_json())
+    try:
+        engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+        # reflect an existing database into a new model
+        Base = automap_base()
+        # reflect the tables
+        Base.prepare(engine, reflect=True)
+        # Save references to each table
+        Station = Base.classes.station
+        Measurement = Base.classes.measurement
+        # Create our session (link) from Python to the DB
+        session = Session(engine)
+        sel_list_temp = [Measurement.date, func.min(Measurement.tobs).label('min_temp'), func.avg(Measurement.tobs).label('avg_temp'), func.max(Measurement.tobs).label('max_temp') ]
+        temp = session.query(*sel_list_temp).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).\
+            group_by(Measurement.date).\
+            order_by(Measurement.date).all()
+        # Transform into dataframe
+        temp_df = pd.DataFrame(temp, columns=["day","Min Temp","Avg Temp","Max Temp"])
+        # update datatype for day
+        temp_df["day"] = pd.to_datetime(temp_df["day"])
+        # update index to handle plot easily
+        temp_df.set_index("day",inplace=True)
+        # See sample data
+        return jsonify({"content":temp_df.to_json()})
+    except:
+        return jsonify({"error":" There is an error with the query. Try agan later"}), 404
 
 
 
